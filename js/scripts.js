b38e333cd71e491b13b7506cf2c18135d27de69c
IwonaMaurer <plastimex2@wp.pl>
2017-10-26 16:45:01 +0200

    pole trójkąta

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 // scripts.js file
-/*  var name = prompt('Enter your name');  */
-/*   alert('Hello, ' + name);   */
-/*console.log('Hello, ' + name);*/
+  /*var name = prompt('Enter your name');  
+  alert('Hello, ' + name);   
+console.log('Hello, ' + name);*/
+
+var a = 6;
+var h = 5;
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert("Pole wynosi " + triangleArea);
