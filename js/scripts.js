ff051e8f6bfb6253544a9ed283d570346e1db18b
IwonaMaurer <plastimex2@wp.pl>
2017-10-27 22:10:12 +0200

    instrukcja if

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,20 @@
   alert('Hello, ' + name);   
 console.log('Hello, ' + name);*/
 
-var a = 6;
+/*var a = 6;
 var h = 5;
 var triangleArea = a*h/2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-alert("Pole wynosi " + triangleArea);
+alert("Pole wynosi " + triangleArea);*/
+
+var a = 5;
+var b = 9;
+var value = (a * a) - (2 * a * b) + (b * b);
+
+if (value > 0 ) {
+   alert('Wynik dodatni');
+} else if (value < 0) {
+   alert('Wynik ujemny');
+} else {
+    alert('Wynik jest równy zero');
+};
