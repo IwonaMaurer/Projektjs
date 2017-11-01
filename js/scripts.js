393a5aa58eafc97e9b64400bfba89d2b478e4bf2
IwonaMaurer <plastimex2@wp.pl>
2017-11-01 18:28:18 +0100

    javascript

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,8 @@ var triangleArea = a*h/2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 alert("Pole wynosi " + triangleArea);*/
 
+
+/*
 var a = 5;
 var b = 9;
 var value = (a * a) - (2 * a * b) + (b * b);
@@ -20,3 +22,16 @@ if (value > 0 ) {
 } else {
     alert('Wynik jest równy zero');
 };
+*/
+
+ function getTriangleArea(a, h) {
+ 	var result = a*h/2;
+  if (a <= 0 || h <= 0) {
+   console.log('Nieprawidłowe dane');
+} else if  (a > 0 || h > 0) {
+   return result;
+};
+}
+var triangle1Area = getTriangleArea(10, 15);
+console.log(getTriangleArea(10, 6));
+
