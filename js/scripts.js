c6a844928e9f9b71e8f8a546e2ae3f907d4d7241
IwonaMaurer <plastimex2@wp.pl>
2017-11-19 10:55:58 +0100

    zadanie11.6

--- js/scripts.js
+++ js/scripts.js
@@ -56,7 +56,7 @@ iPhone6.logModel();
 */
 
 // zadanie 11.5
-function Button(text) {
+/*function Button(text) {
      this.text = text || 'Hello';
    }
       Button.prototype = {
@@ -73,3 +73,149 @@ function Button(text) {
     };
         var btn1 = new Button('Hello');
         btn1.create();
+        */
+
+
+//zadanie 11.6
+$(function(){
+
+  function randomString() {
+
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    var str = '';
+    for (var i = 0; i < 10; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+  }
+
+
+
+  function Column(name) {
+    var self = this;
+
+    this.id = randomString();
+    this.name = name;
+    this.$element = createColumn();
+
+    function createColumn() {
+
+     // Create components
+      var $column = $('<div>').addClass('column');
+      var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+      var $columnCardList = $('<ul>').addClass('column-card-list');
+      var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+      var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+
+
+      //adding events
+      $columnDelete.click(function() {
+        self.removeColumn();
+      });
+      //Add a note after clickin on the button
+      $columnAddCard.click(function() {
+        self.addCard(new Card(prompt("Enter the name of the card")));
+      });
+      //Construction column element
+      $column.append($columnTitle)
+             .append($columnDelete)
+             .append($columnAddCard)
+             .append($columnCardList)
+      // return of created column
+      return $column;
+    }
+
+  }  //function column koniec
+  Column.prototype = {
+     addCard: function(card) {
+      this.$element.children('ul').append(card.$element);
+     },
+     removeColumn: function() {
+      this.$element.remove();
+     }
+  };
+  //metoda addCard
+  this.$element.children('ul').append(card.$element);
+  // remove column
+  this.$element.remove();
+
+  //funkcja konstruująca klasę card
+
+  function card(description) {
+        var self = this;
+
+        this.id = randomString();
+        this.description = description;
+        this.$element = createCard();
+
+        function createCard() {
+              var $card = $('<li>').addClass('card');
+              var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+              var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+
+             //usunięcie karty
+             $cardDelete.click(function() {
+              self.removeCard();
+             });
+
+             //konstruowanie karty
+             $card.append($cardDelete)
+                   .append($cardDescription);
+
+                   return $card;
+           }
+  }
+       card.prototype = {
+        removeCard: function() {
+          this.$element.remove();
+        }
+       }
+
+       var board = {
+        name: 'Kanban Board',
+        //stworzenie kolumny
+        addColumn: function(column) {
+          this.$element.append(column.$element);
+          initSortable();
+        },
+        $element: $('#board .column-container')
+
+       };
+
+       function initSortable() {
+        $('.column-card-list').sortable({
+          connectWith: '.column-card-list',
+          placeholder: 'card-placeholder'
+        }).disableSelection();
+       }
+       //wrzucanie nowej kolumny do tablicy
+       $('.create-column')
+          .click(function(){
+            var name = prompt('Enter a column name');
+            var column = new Column(name);
+                board.addColumn(column);
+          });
+
+          // tworzenie kolumn
+          var todoColumn = new Column('To do');
+          var doingColumn = new Column('Doing');
+          var doneColumn = new Column('Done');
+
+         // Dodawanie kolumn
+         board.addColumn(todoColumn);
+         board.addColumn(doingColumn);
+         board.addColumn(doneColumn);
+
+         //tworzenie kart
+         var card1 = new Card('New task');
+         var card2 = new Card('Create kanban boards');
+
+         //dodanie karty do kolumny
+         todoColumn.addCard(card1);
+         doingColumn.addCard(card2);
+
+
+})
+
+
+
