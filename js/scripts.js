1702db5c0e500a8cae0340eaf6798354433d0526
IwonaMaurer <plastimex2@wp.pl>
2017-11-23 20:35:22 +0100

    zadanie 12.5

--- js/scripts.js
+++ js/scripts.js
@@ -77,6 +77,8 @@ iPhone6.logModel();
 
 
 //zadanie 11.6
+
+/*
 $(function() {
 
   function randomString() {
@@ -212,6 +214,10 @@ $(function() {
 
 
 })
+*/
+
+
+/*
 //zadanie 12.4
 
 var url = 'http://api.icndb.com/jokes/random';
@@ -230,5 +236,40 @@ function getJoke() {
   });
   xhr.send();
 }
+*/
 
+//zadanie 12.5
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
+function getQuote() {
+  $.getJSON(prefix + quoteUrl, createTweet);
+  $.ajaxSetup({ cache: false });
+}
+function createTweet(input) {
+  var data = input[0];
+  var quoteText = $(data.content).text().trim();
+  var quoteAuthor = data.title;
+
+  if (!quoteAuthor.length) {
+    quoteAuthor = "Unknown author";
+  }
+  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+  if (tweetText.length > 140) {
+      getQuote();
+  } else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    $('.quote').text(quoteText);
+    $('.author').text("Author: " + quoteAuthor);
+    $('.tweet').attr('href', tweet);
+  }
+ }
+ 
+$(document).ready(function() {
+  getQuote();
+  $('.trigger').click(function() {
+    getQuote();
+  })
+});
